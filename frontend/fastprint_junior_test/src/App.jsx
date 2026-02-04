47df011deeb2a31ca51de5250ed70fe6eb5530d9
bagoesihsant <bagoesihsant@gmail.com>
2026-02-04 15:59:08 +0700

    Create Add Produk UI.

--- frontend/fastprint_junior_test/src/App.jsx
+++ frontend/fastprint_junior_test/src/App.jsx
@@ -1,34 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [isAdd, setIsAdd] = useState(false);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='bg-gray-900 h-screen grid grid-cols-12'>
+      <div className="col-span-6 col-start-4">
+        <h1 className='text-gray-50 text-3xl text-center my-12 font-bold'>Produk Fast Print</h1>
+        <button type="button" className="border-2 bg-slate-300 rounded text-center text-gray-900 font-semibold col-span-8 col-start-3 py-2 px-2 cursor-pointer hover:bg-slate-900 hover:border-slate-300 hover:text-gray-50" onClick={() => { setIsAdd(!isAdd); }}>{ !isAdd ? "Tambah Produk" : "Batal Tambah Produk" }</button>
+        { isAdd && (
+          <form method="POST">
+            <div className="my-6">
+                <h2 className="text-gray-50 text-center font-semibold text-3xl">Tambah Produk</h2>
+                <div className="my-4 grid grid-cols-12 gap-2">
+                  <label htmlFor="nama_produk" className='text-gray-50 col-span-4 font-semibold'>Nama Produk</label>
+                  <input type="text" name="nama_produk" id="nama_produk" className='border-2 border-gray-400 py-1 indent-2 rounded text-gray-50 col-span-12' />
+                </div>
+                <div className="my-4 grid grid-cols-12 gap-2">
+                  <label htmlFor="harga_produk" className='text-gray-50 col-span-4 font-semibold'>Harga Produk</label>
+                  <input type="text" name="harga_produk" id="harga_produk" className='border-2 border-gray-400 py-1 indent-2 rounded text-gray-50 col-span-12' />
+                </div>
+                <div className="my-4 grid grid-cols-12 gap-2">
+                  <label htmlFor="kategori_produk" className='text-gray-50 col-span-4 font-semibold'>Kategori</label>
+                  <select name="kategori_produk" id="kategori_produk" className='border-2 border-gray-400 py-1 indent-2 rounded text-gray-50 col-span-12'>
+                    <option value="1" className='bg-gray-900'>L QUEENLY</option>
+                    <option value="2" className='bg-gray-900'>L MTH AKSESORIS (IM)</option>
+                    <option value="3" className='bg-gray-900'>L MTH TABUNG (LK)</option>
+                  </select>
+                </div>
+                <div className="my-4 grid grid-cols-12 gap-2">
+                  <label htmlFor="status_produk" className='text-gray-50 col-span-4 font-semibold'>Status</label>
+                  <select name="status_produk" id="status_produk" className='border-2 border-gray-400 py-1 indent-2 rounded text-gray-50 col-span-12'>
+                    <option value="1" className='bg-gray-900'>Bisa Dijual</option>
+                    <option value="2" className='bg-gray-900'>Tidak Bisa Dijual</option>
+                  </select>
+                </div>
+                <div className="my-6 grid grid-cols-12 gap-2">
+                  <button type="submit" className='border-2 bg-slate-300 rounded text-center text-gray-900 font-semibold col-span-8 col-start-3 py-2 cursor-pointer hover:bg-slate-900 hover:border-slate-300 hover:text-gray-50'>Tambah Produk</button>
+                </div>
+            </div>
+          </form>
+        ) }
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
